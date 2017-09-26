d1c091b0a4e275af6280a368baeed598cc242b08
Wanda Ichsanul Isra <wandaisra@gmail.com>
2017-09-26 16:58:15 +0700

    Add register activity

---
+++ app/src/main/java/io/devsummit/devsummit/Activities/RegisterActivity.java
@@ -0,0 +1,36 @@
+package io.devsummit.devsummit.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import io.devsummit.devsummit.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    private void startFormRegisterActivity(String registerWith) {
+        Intent intent = new Intent(this, FormRegisterActivity.class);
+        intent.putExtra("registerWith", registerWith);
+        startActivity(intent);
+    }
+
+    public void clickRegisterWithEmail(View view) {
+        String registerWith = "email";
+        startFormRegisterActivity(registerWith);
+    }
+
+    public void clickRegisterWithPhone(View view) {
+        String registerWith = "phone";
+        startFormRegisterActivity(registerWith);
+    }
+
+    public void clickSignIn(View view) {
+    }
+}
