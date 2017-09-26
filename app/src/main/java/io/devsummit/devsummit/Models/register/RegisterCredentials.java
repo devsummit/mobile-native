7d02cfc358f2b4151e2bd2794a74e4bf3e1c6628
Wanda Ichsanul Isra <wandaisra@gmail.com>
2017-09-26 16:59:06 +0700

    Add register credentials model

---
+++ app/src/main/java/io/devsummit/devsummit/Models/register/RegisterCredentials.java
@@ -0,0 +1,15 @@
+package io.devsummit.devsummit.Models.register;
+
+/**
+ * Created by wlisrausr on 25/09/17.
+ */
+
+public class RegisterCredentials
+{
+    public String first_name;
+    public String last_name;
+    public String email;
+    public String username;
+    public String password;
+    public String referer;
+}
