d09d560e5301754d890bbdf65b42bf49b461fd93
Wanda Ichsanul Isra <wandaisra@gmail.com>
2017-09-28 00:05:45 +0700

    Add register mobile API service

--- app/src/main/java/io/devsummit/devsummit/Remote/APIService.java
+++ app/src/main/java/io/devsummit/devsummit/Remote/APIService.java
@@ -5,6 +5,7 @@ import io.devsummit.devsummit.Models.RegisterModel;
 import io.devsummit.devsummit.Models.login.Credentials;
 import io.devsummit.devsummit.Models.login.MobileCredentials;
 import io.devsummit.devsummit.Models.register.RegisterCredentials;
+import io.devsummit.devsummit.Models.register.RegisterMobileCredentials;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Headers;
@@ -22,6 +23,10 @@ public interface APIService {
     @POST("auth/login")
     @Headers("Content-Type:application/json")
     Call<LoginModel> loginMobile(@Body MobileCredentials credentials);
+
+    @POST("auth/register")
+    @Headers("Content-Type:application/json")
+    Call<RegisterModel> registerMobile(@Body RegisterMobileCredentials credentials);
 
     @POST("auth/register")
     @Headers("Content-Type:application/json")
