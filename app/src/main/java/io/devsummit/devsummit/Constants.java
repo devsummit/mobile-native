9c187a29c976b63e61f0550aa661dc05a09f1173
caudaganesh <caudaganesh@gmail.com>
2017-09-29 13:02:25 +0700

    add constants

---
+++ app/src/main/java/io/devsummit/devsummit/Constants.java
@@ -0,0 +1,13 @@
+package io.devsummit.devsummit;
+
+/**
+ * Created by ganesh on 27/09/17.
+ */
+
+public interface Constants {
+    public static final String USER_AUTH = "User Authorization";
+    public static final String USER_PROFILE = "User Profile";
+    public static final String REFRESH_TOKEN = "RefreshToken";
+    public static final String ACCESS_TOKEN = "AccessToken";
+}
+
