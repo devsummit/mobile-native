0e4ea0f0c82061150aac5338c3f191bc1cc47cfc
caudaganesh <caudaganesh@gmail.com>
2017-09-29 13:09:37 +0700

    add refresh token, notif, userticket apiservice

--- app/src/main/java/io/devsummit/devsummit/Remote/APIService.java
+++ app/src/main/java/io/devsummit/devsummit/Remote/APIService.java
@@ -2,14 +2,20 @@ package io.devsummit.devsummit.Remote;
 
 import io.devsummit.devsummit.Models.LoginModel;
 import io.devsummit.devsummit.Models.RegisterModel;
+import io.devsummit.devsummit.Models.UserTicketModel;
 import io.devsummit.devsummit.Models.login.Credentials;
 import io.devsummit.devsummit.Models.login.MobileCredentials;
 import io.devsummit.devsummit.Models.register.RegisterCredentials;
 import io.devsummit.devsummit.Models.register.RegisterMobileCredentials;
+import io.devsummit.devsummit.Models.NotificationModel;
+import io.devsummit.devsummit.Models.authmodel.RefreshTokenModel;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
+import retrofit2.http.Url;
 
 /**
  * Created by ganesh on 25/09/17.
@@ -31,4 +37,18 @@ public interface APIService {
     @POST("auth/register")
     @Headers("Content-Type:application/json")
     Call<RegisterModel> register(@Body RegisterCredentials credentials);
+    @GET("api/v1/notifications?page=1")
+    Call<NotificationModel> initialFetchNotification(@Header("Authorization") String token);
+
+    @GET
+    @Headers("Content-Type: application/json")
+    Call<NotificationModel> fetchNotification(@Header("Authorization") String token, @Url String url);
+
+    @POST
+    @Headers("Content-Type:application/json")
+    Call<RefreshTokenModel> refreshToken(@Body String refresh_token);
+
+    @GET
+    Call<UserTicketModel> fetchUserTicket(@Header("Authorization") String token);
+
 }
