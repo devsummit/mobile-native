a93052f43c6745ff6c1d536fc942b3c3433581dd
caudaganesh <caudaganesh@gmail.com>
2017-09-29 18:21:07 +0700

    add recycler view ticket list

--- app/src/main/java/io/devsummit/devsummit/Fragments/TicketFragment.java
+++ app/src/main/java/io/devsummit/devsummit/Fragments/TicketFragment.java
@@ -3,26 +3,38 @@ package io.devsummit.devsummit.Fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import io.devsummit.devsummit.Controllers.UserTicketController;
+import io.devsummit.devsummit.Helpers.User_Authentication_Helper;
+import io.devsummit.devsummit.Models.UserTicketModel;
 import io.devsummit.devsummit.R;
+import io.devsummit.devsummit.ViewAdapters.TicketListViewAdapter;
 
 /**
  * Created by ganesh on 27/09/17.
  */
 
 public class TicketFragment extends Fragment {
-    private static final String ARG_TEXT = "arg_text";
-    private static final String ARG_COLOR = "arg_color";
-
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
     private View mContent;
+    private UserTicketController userTicketController;
+    private User_Authentication_Helper authHelper;
+    private static final String USER_TICKETS = "user_ticket_model";
+    private  UserTicketModel mUserTicketModel;
 
-    public static Fragment newInstance() {
+    public static Fragment newInstance(UserTicketModel userTicketModel) {
         Fragment frag = new TicketFragment();
-        Bundle args = new Bundle();
-        frag.setArguments(args);
+
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(USER_TICKETS, userTicketModel);
+        frag.setArguments(bundle);
         return frag;
     }
 
@@ -30,19 +42,42 @@ public class TicketFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        mUserTicketModel = (UserTicketModel) getArguments().getSerializable(
+                USER_TICKETS);
+
+
+        View layout= inflater.inflate(R.layout.ticket_fragment, container, false);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.ticket_fragment, container, false);
+        authHelper = new User_Authentication_Helper(getActivity());
+
+
+        mRecyclerView = (RecyclerView) layout.findViewById(R.id.user_ticket_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        // mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // get ticket data
+
+        // specify an adapter (see also next example)
+        mAdapter = new TicketListViewAdapter(mUserTicketModel.getData());
+        mRecyclerView.setAdapter(mAdapter);
+
+        return layout;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-
-
         // initialize views
         mContent = view.findViewById(R.id.ticket_content);
-
     }
 
     @Override
