8bf86c4bdfa092d5902b5517323c385018d519d7
caudaganesh <caudaganesh@gmail.com>
2017-09-29 13:03:23 +0700

    add bottom navigation and fragment template

--- app/src/main/java/io/devsummit/devsummit/Activities/MainActivity.java
+++ app/src/main/java/io/devsummit/devsummit/Activities/MainActivity.java
@@ -1,20 +1,56 @@
 package io.devsummit.devsummit.Activities;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.view.MenuItem;
+
+import io.devsummit.devsummit.Fragments.FeedFragment;
+import io.devsummit.devsummit.Fragments.TicketFragment;
+import io.devsummit.devsummit.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment frag = null;
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    frag = FeedFragment.newInstance();
+                    break;
+                case R.id.tickets:
+                    frag = TicketFragment.newInstance();
+                    break;
+            }
+
+            if (frag != null) {
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.add(R.id.container, frag, frag.getTag());
+                ft.commit();
+            }
+
+            return true;
+        }
+
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        startActivity(new Intent(this, LoginActivity.class));
-    }
 
-    public void clickRegister(View view) {
-        Intent intent = new Intent(this, RegisterActivity.class);
-        startActivity(intent);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        Fragment frag = TicketFragment.newInstance();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.container, frag, frag.getTag());
+        ft.commit();
     }
 }
