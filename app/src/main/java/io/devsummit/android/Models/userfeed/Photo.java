c387237148332337b24813b0c9b5b37fe6b4dc87
Wanda Ichsanul Isra <wandaisra@gmail.com>
2017-10-09 18:12:20 +0700

    Add photo user feed model

---
+++ app/src/main/java/io/devsummit/android/Models/userfeed/Photo.java
@@ -0,0 +1,69 @@
+package io.devsummit.android.Models.userfeed;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class Photo extends RealmObject implements Serializable {
+
+    @SerializedName("created_at")
+    @Expose
+    private String createdAt;
+    @SerializedName("id")
+    @Expose
+    @PrimaryKey
+    private Integer id;
+    @SerializedName("updated_at")
+    @Expose
+    private String updatedAt;
+    @SerializedName("url")
+    @Expose
+    private String url;
+    @SerializedName("user_id")
+    @Expose
+    private Integer userId;
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+}
