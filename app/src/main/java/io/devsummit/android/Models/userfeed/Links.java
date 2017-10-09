a64f6ff8c9406e892bb26a3308b01a4a28c7cd39
Wanda Ichsanul Isra <wandaisra@gmail.com>
2017-10-09 18:12:07 +0700

    Add links user feed model

---
+++ app/src/main/java/io/devsummit/android/Models/userfeed/Links.java
@@ -0,0 +1,52 @@
+package io.devsummit.android.Models.userfeed;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Links {
+
+    @SerializedName("curr")
+    @Expose
+    private String curr;
+    @SerializedName("next")
+    @Expose
+    private Object next;
+    @SerializedName("prev")
+    @Expose
+    private String prev;
+    @SerializedName("total_items")
+    @Expose
+    private Integer totalItems;
+
+    public String getCurr() {
+        return curr;
+    }
+
+    public void setCurr(String curr) {
+        this.curr = curr;
+    }
+
+    public Object getNext() {
+        return next;
+    }
+
+    public void setNext(Object next) {
+        this.next = next;
+    }
+
+    public String getPrev() {
+        return prev;
+    }
+
+    public void setPrev(String prev) {
+        this.prev = prev;
+    }
+
+    public Integer getTotalItems() {
+        return totalItems;
+    }
+
+    public void setTotalItems(Integer totalItems) {
+        this.totalItems = totalItems;
+    }
+}
