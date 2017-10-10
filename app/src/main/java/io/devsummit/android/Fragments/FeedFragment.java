3d6f06e4b3a9d43d9d1f85462e2b3d316fa984af
Wanda Ichsanul Isra <wandaisra@gmail.com>
2017-10-10 12:56:23 +0700

    Remove feed fragment & add attach adapter

--- app/src/main/java/io/devsummit/android/Fragments/FeedFragment.java
+++ app/src/main/java/io/devsummit/android/Fragments/FeedFragment.java
@@ -11,7 +11,7 @@ import android.view.ViewGroup;
 
 import java.util.List;
 
-import io.devsummit.android.Controllers.UserFeedController;
+import io.devsummit.android.Activities.MainActivity;
 import io.devsummit.android.Helpers.EndlessRecyclerViewScrollListener;
 import io.devsummit.android.Helpers.UserAuthenticationHelper;
 import io.devsummit.android.Models.userfeed.FeedData;
@@ -25,73 +25,64 @@ import io.realm.Sort;
  */
 
 public class FeedFragment extends Fragment {
-    private static final String USER_FEED = "user_feed_model";
-    private View mContent;
     private UserAuthenticationHelper authHelper;
     private RecyclerView mRecyclerView;
     private LinearLayoutManager mLayoutManager;
     private RecyclerView.Adapter mAdapter;
-    private EndlessRecyclerViewScrollListener scrollListener;
-    private UserFeedController userFeedController;
     private List<FeedData> feedData;
-
-    public static Fragment newInstance() {
-        Fragment frag = new FeedFragment();
-        return frag;
-    }
+    private EndlessRecyclerViewScrollListener scrollListener;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        Realm realm = Realm.getDefaultInstance();
-        feedData = realm.where(FeedData.class).findAllSorted("createdAt", Sort.DESCENDING);
+        authHelper = new UserAuthenticationHelper(getActivity());
+        String token = authHelper.getAccessToken();
+        MainActivity.userFeedController.getUserFeed(token, getContext(), 1);
 
         // Inflate the layout for this fragment
         View layout = inflater.inflate(R.layout.feed_fragment, container, false);
 
-        authHelper = new UserAuthenticationHelper(getActivity());
-        userFeedController = new UserFeedController(null);
+        mRecyclerView = (RecyclerView) layout.findViewById(R.id.feeds_recycler_view);
 
-        if (feedData.size() > 0) {
-            mRecyclerView = (RecyclerView) layout.findViewById(R.id.feeds_recycler_view);
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
 
-            // use a linear layout manager
-            mLayoutManager = new LinearLayoutManager(getContext());
-            mRecyclerView.setLayoutManager(mLayoutManager);
+        //attachAdapter();
 
-            // specify an adapter
-            mAdapter = new FeedListViewAdapter(getContext(), feedData);
-            mRecyclerView.setAdapter(mAdapter);
+        scrollListener = new EndlessRecyclerViewScrollListener(mLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                loadNextDataFromApi(page, totalItemsCount);
+            }
+        };
 
-            scrollListener = new EndlessRecyclerViewScrollListener(mLayoutManager) {
-                @Override
-                public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
-                    loadNextDataFromApi(page, totalItemsCount);
-                }
-            };
-
-            mRecyclerView.addOnScrollListener(scrollListener);
-        }
+        mRecyclerView.addOnScrollListener(scrollListener);
 
         return layout;
+    }
+
+    public void attachAdapter(){
+        Realm realm = Realm.getDefaultInstance();
+        feedData = realm.where(FeedData.class).findAllSorted("createdAt", Sort.DESCENDING);
+
+        // specify an adapter (see also next example)
+        mAdapter = new FeedListViewAdapter(feedData, getContext());
+        mRecyclerView.setAdapter(mAdapter);
     }
 
     public void loadNextDataFromApi(int offset, int totalItems) {
         // Send an API request to retrieve appropriate paginated data
         String token = authHelper.getAccessToken();
-
         offset = totalItems / 10;
 
-        userFeedController.getUserFeed(token, getContext(), offset + 1);
+        MainActivity.userFeedController.getUserFeed(token, getContext(), offset + 1);
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        // initialize views
-        mContent = view.findViewById(R.id.feed_content);
     }
 
     @Override
